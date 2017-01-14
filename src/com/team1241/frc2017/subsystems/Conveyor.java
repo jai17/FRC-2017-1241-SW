49580ce5073018cdef4b5c511f8d57b9fffbaf35
Kaveesha Siribaddana <kavee360@gmail.com>
2017-01-14 16:11:07 -0500

    Conveyor

--- src/com/team1241/frc2017/subsystems/Conveyor.java
+++ src/com/team1241/frc2017/subsystems/Conveyor.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Conveyor extends Subsystem {
     
-    // Put methods for controlling this subsystem
+    //Put methods for controlling this subsystem
     // here. Call these from Commands.
 	// Victors
 
