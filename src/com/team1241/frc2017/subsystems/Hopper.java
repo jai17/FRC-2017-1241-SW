4ee49cb363388e54f4d39139ab7249d40f80e4a5
hsiddiqui99 <h.siddiqui99@outlook.com>
2017-01-17 16:35:50 -0500

    Changed Conveyor and added hooper subsystem

---
+++ src/com/team1241/frc2017/subsystems/Hopper.java
@@ -0,0 +1,28 @@
+package com.team1241.frc2017.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Hopper extends Subsystem {
+    
+	//*********************************************************************************************
+	//*********************************** VARIABLES ***********************************************
+	//*********************************************************************************************
+	
+	DoubleSolenoid hopper;
+	DoubleSolenoid gear;
+	
+	public Hopper();
+	
+	hopper = new DoubleSolenoid(ElectricalConstants.)	
+	
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
